832e8a65cc1186496c69afc37e8e179d381d1050
zhouyanpu <zhouyanpu@gmail.com>
2019-07-29 12:25:02 -0400

    Updates

---
+++ precache-manifest.875632b72ba9c8b0cac7c7442d6fe966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa266dc51cdbdfb864e650852619367",
+    "url": "/Pixelate-React/index.html"
+  },
+  {
+    "revision": "5cac21f42694d891e615",
+    "url": "/Pixelate-React/static/css/main.08f0067f.chunk.css"
+  },
+  {
+    "revision": "531013d8278e824b63bd",
+    "url": "/Pixelate-React/static/js/2.796d69b7.chunk.js"
+  },
+  {
+    "revision": "5cac21f42694d891e615",
+    "url": "/Pixelate-React/static/js/main.c742d716.chunk.js"
+  },
+  {
+    "revision": "2f621b00320938f00e54",
+    "url": "/Pixelate-React/static/js/runtime~main.507451a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pixelate-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
